0b100e425ae6ef98bb2d2386ef34b884537b81a9
tulgaay <123799181+tulgaay@users.noreply.github.com>
2023-03-06 00:40:14 +0300

    Delete AylanurTulgaHW4/AylanurTulgaHW4 directory

--- AylanurTulgaHW4/AylanurTulgaHW4/src/test/java/edu/sabanciuniv/AylanurTulgaHW4/repository/studentRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.sabanciuniv.AylanurTulgaHW4.repository;
-
-import edu.sabanciuniv.AylanurTulgaHW4.model.Student;
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface studentRepository extends JpaRepository<Student, Integer> {
-}
